2624c62c566b7f4badaa4af5ac88907445f52f73
luyong <luyong@nationalchip.com>
2026-07-10 21:35:08 +0800

    disable alt+enter keybind

--- config/bingdings.lua
+++ config/bingdings.lua
@@ -124,7 +124,7 @@ local keys = {
    },
 
    -- panes: zoom+close pane
-   { key = 'Enter', mods = mod.SUPER,     action = act.TogglePaneZoomState },
+   -- { key = 'Enter', mods = mod.SUPER,     action = act.TogglePaneZoomState },
    { key = 'w',     mods = mod.SUPER,     action = act.CloseCurrentPane({ confirm = false }) },
 
    -- panes: navigation
